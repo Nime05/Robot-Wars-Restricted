0f6c104898d736c53e41761c283ae0a59ef8cce5
Mestrup <nico.mestrup@btc-ag.com>
2024-09-23 14:37:14 +0200

    First commit

---
+++ src/main/java/com/btcag/bootcamp/Clear.java
@@ -0,0 +1,11 @@
+package com.btcag.bootcamp;
+
+public class Clear {
+    public static void clear() {
+        int i = 0;
+        while (i < 50) {
+            System.out.println();
+            i++;
+        }
+    }
+}
---
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -0,0 +1,75 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+import static com.btcag.bootcamp.Clear.clear;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte gib deinen Spielernamen ein:");
+        String playername = scanner.nextLine();
+        clear();
+        System.out.println("Herzlich Willkommen bei Robot Wars " + playername + " !\n");
+
+        System.out.println("Benenne bitte deinen Roboter:");
+        String botname = scanner.nextLine();
+
+        int robotX = 16;
+        int robotY = 11;
+
+        boolean newcords = true;
+
+        while (newcords) {
+            System.out.println("Bitte gib die X-Koordinate an (1-15):");
+            int currentX = scanner.nextInt();
+            while(currentX < 1 || currentX > 15)
+            {
+                System.out.println("Ungültige Eingabe! Bitte gib eine Zahl zwischen 1-15 an:");
+                currentX = scanner.nextInt();
+            }
+
+            clear();
+
+            System.out.println("Bitte gib die Y-Koordinate an (1-10):");
+            int currentY = scanner.nextInt();
+            while(currentY < 1 || currentY > 10)
+            {
+                System.out.println("Ungültige Eingabe! Bitte gib eine Zahl zwischen 1-10 an:");
+                currentY = scanner.nextInt();
+            }
+
+
+            clear();
+            int y = 1;
+            while (y < robotY) {
+                int x = 1;
+                while (x < robotX) {
+
+                    boolean isrobotposition = false;
+
+                    while (x == currentX && y == currentY) {
+                        System.out.print("[ X ]");
+                        isrobotposition = true;
+                        break;
+                    }
+
+
+                    while (!isrobotposition) {
+                        System.out.print("[   ]");
+                        break;
+                    }
+
+                    x++;
+                }
+                System.out.println();
+                y++;
+            }
+            System.out.println(botname+" ist gerade bei X: "+currentX+" Y: "+currentY+" .\n\n");
+
+            System.out.println("Möchtest du neue Koordinaten eingeben? (ja/nein)");
+            String answer = scanner.next();
+            newcords = answer.equalsIgnoreCase("ja");
+        }
+    }
+}
